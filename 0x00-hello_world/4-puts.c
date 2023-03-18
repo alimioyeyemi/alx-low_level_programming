00b56dd007fe780554cbbd3aedb895c02c6b9b62
Oyeyemi Alimi <yemmyyemyte@gmail.com>
2023-03-18 05:18:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts functio
-n
+* main - A program that prints a line with puts function
 * Return:0 (Success)
 */
 int main(void)
 {
-puts("\"programming is like builProgramming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
