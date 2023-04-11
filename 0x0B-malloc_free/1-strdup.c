9779da97045a15ff163738130e4701209013a223
Oyeyemi Alimi <yemmyyemyte@gmail.com>
2023-04-11 15:26:28 -0700

    yes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,40 +18,25 @@ char *_strdup(char *str)
 		
 {
 		
-	char *aaa;
+char *aaa;
 		
-	int i, r = 0;
-		
-
-		
-	if (str == NULL)
-		
-		return (NULL);
-		
-	i = 0;
-		
-	while (str[i] != '\0')
-		
-		i++;
+int i, r = 0;
 		
 
+if (str == NULL)
 		
-	aaa = malloc(sizeof(char) * (i + 1));
+return (NULL);
 		
-
+i = 0;
 		
-	if (aaa == NULL)
+while (str[i] != '\0')
+i++;
+aaa = malloc(sizeof(char) * (i + 1));
 		
-		return (NULL);
-		
-
-		
-	for (r = 0; str[r]; r++)
-		
-		aaa[r] = str[r];
-		
-
-		
-	return (aaa);
+if (aaa == NULL)
+	return (NULL);
+for (r = 0; str[r]; r++)
+aaa[r] = str[r];
+return (aaa);
 		
 }
